3d78df0b906362225d3f18f643d1af8248b1ef32
Alejandro Pinola <alepinola@gmail.com>
2021-12-13 20:53:14 -0300

    navigation based on subscription states

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useEffect, useState } from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import PrivateRoute from './components/PrivateRoute';
 import Admin from './components/Admin';
 import Subscriber from './components/Subscriber';
-import { SmartContractContextProvider } from './context/SmartContractContext';
+import { useAccount, useSubscriptionContext } from './context/SmartContractContext';
 
 const App: FC = () => {
+  const contract = useSubscriptionContext();
+  const account = useAccount();
+  const [isOwner, setIsOwner] = useState(true);
+  const [isSubscribed, setIsSubscribed] = useState(true);
+
+  const checkAccount = useCallback(async () => {
+    const checkIfIsOwner = async (): Promise<boolean> => {
+      const contractData = await contract.getAllContractData(account);
+      if (contractData.owner.toUpperCase() === account.toUpperCase()) {
+        return true;
+      }
+      return false;
+    };
+
+    const checkIfIsSubscribed = async (): Promise<boolean> => {
+      return contract.amISubscribed(account);
+    };
+    if (!account) return;
+    const ownership = await checkIfIsOwner();
+    const subscription = await checkIfIsSubscribed();
+    console.log({ ownership, subscription });
+    setIsOwner(ownership);
+    setIsSubscribed(subscription);
+  }, [account, contract]);
+
+  useEffect(() => {
+    checkAccount();
+  }, [checkAccount]);
+
   return (
     <div className="App">
-      <SmartContractContextProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<PrivateRoute authCondition={isOwner} />}>
             <Route path="/admin" element={<Admin />} />
+          </Route>
+          <Route path="/subscriber" element={<PrivateRoute authCondition={isSubscribed} />}>
             <Route path="/subscriber" element={<Subscriber />} />
-          </Routes>
-        </BrowserRouter>
-      </SmartContractContextProvider>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,33 +1,11 @@
-import React, { FC, useState, useEffect, useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { FC } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import MetaMaskIcon from './MetaMaskIcon';
-import { useMetaMask, useSubscriptionContext, useAccount } from '../context/SmartContractContext';
+import { useMetaMask, useAccount } from '../context/SmartContractContext';
 
 const Home: FC = () => {
-  const navigate = useNavigate();
   const connect = useMetaMask();
-  const subscriptionContract = useSubscriptionContext();
   const account = useAccount();
-  const [isConnected, setIsConnected] = useState(false);
-
-  const checkAccount = useCallback(async () => {
-    if (!account) return;
-    const contractData = await subscriptionContract.getAllContractData(account);
-    if (contractData.owner === account) {
-      navigate('/admin');
-    } else {
-      const amISubscribed = await subscriptionContract.amISubscribed(account);
-      if (amISubscribed) {
-        navigate('/subscriber');
-      }
-    }
-    setIsConnected(true);
-  }, [account, navigate, subscriptionContract]);
-
-  useEffect(() => {
-    checkAccount();
-  }, [checkAccount]);
 
   return (
     <Modal show centered>
@@ -38,7 +16,7 @@ const Home: FC = () => {
         Contribute with just <b>0.001 ETH</b> and get access to awesome content.
       </Modal.Body>
       <Modal.Footer className="d-flex justify-content-center">
-        {isConnected ? (
+        {account.length ? (
           <>
             Connected with wallet <b>{account}</b>
             <Button variant="secondary" className="d-flex">
